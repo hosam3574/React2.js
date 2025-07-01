5b1a439a677c3ca1386577f80d6b95b23841ec4b
hosam3574 <hossamtradat@gmail.com>
2025-07-01 18:40:57 +0300

    edit

--- my-app/src/componants/Categories.js
+++ my-app/src/componants/Categories.js
@@ -37,11 +37,16 @@ return(
 
 </div>
 
+
+<div className="allphoto">
+
+
+
 <div>
     <button  className="photo1">
-<img src="https://www.botnia.in/cdn/shop/files/5_41b6d8fa-fa23-4550-97f2-5161b85abcbd.png?v=1695274048" alt=""></img>
+<img className="im" src="https://www.botnia.in/cdn/shop/files/5_41b6d8fa-fa23-4550-97f2-5161b85abcbd.png?v=1695274048" alt=""></img>
 
-<h1> Vetements pour hommes </h1>
+<h1 className="write"> Vetements pour hommes </h1>
     </button>
 </div>
 
@@ -49,8 +54,70 @@ return(
 
 
 
+<div>
+
+<button className="photo2">
+<img className="im2" src="https://png.pngtree.com/png-vector/20230906/ourmid/pngtree-wristwatch-analog-classic-brown-leather-strap-watch-png-image_10001801.png"></img>
+
+<h1 className="write2">Montres</h1>
+</button>
 
 
+
+</div>
+
+
+
+<div>
+
+<button className="photo2" >
+<img className="im3" src="https://img.freepik.com/free-vector/realistic-make-up-brushes-composition_1284-25929.jpg"></img>
+<h1  className="write2"> Sante et beautu  </h1>
+</button>
+</div>
+
+
+<div>
+
+<button className="photo2">
+<img className="im2" src="https://png.pngtree.com/png-vector/20230906/ourmid/pngtree-wristwatch-analog-classic-brown-leather-strap-watch-png-image_10001801.png"></img>
+
+<h1 className="write2">Montres</h1>
+</button>
+</div>
+
+<div>
+<button className="photo2" >
+<img className="im3" src="https://img.freepik.com/free-vector/realistic-make-up-brushes-composition_1284-25929.jpg"></img>
+<h1  className="write2"> Sante et beautu  </h1>
+</button>
+
+</div>
+
+<div>
+
+    <button className="photo2">
+<img className="im2" src="https://png.pngtree.com/png-vector/20230906/ourmid/pngtree-wristwatch-analog-classic-brown-leather-strap-watch-png-image_10001801.png"></img>
+
+<h1 className="write2">Montres</h1>
+</button>
+
+</div>
+
+<div>
+    <button className="photo2" >
+<img className="im3" src="https://img.freepik.com/free-vector/realistic-make-up-brushes-composition_1284-25929.jpg"></img>
+<h1  className="write2"> Sante et beautu  </h1>
+</button>
+</div>
+
+
+
+</div>
+
+<h1 className="color">
+    DECOUVERTES QUOTIDIENNE
+</h1>
 
 </div>
 
